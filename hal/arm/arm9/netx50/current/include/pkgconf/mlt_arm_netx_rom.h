7269a7b522f61b36acc33c3e71705756cdf997b2
mtrensch <mtrensch@ef330a71-0d16-40ee-bf2b-888bb217d0c2>
2010-05-31 05:55:51 +0000

    Added CYG_HAL_STARTUP_ROM mode (XiP from parallel flash)
    git-svn-id: svn://netx01/ecos/netx_bsp/trunk@19 ef330a71-0d16-40ee-bf2b-888bb217d0c2

--- hal/arm/arm9/netx50/current/include/pkgconf/mlt_arm_netx_rom.h
+++ hal/arm/arm9/netx50/current/include/pkgconf/mlt_arm_netx_rom.h
@@ -7,11 +7,15 @@
 #include <stddef.h>
 
 #endif
+
 #define CYGMEM_REGION_ram (0x80000000)
-#define CYGMEM_REGION_ram_SIZE (0x04000000)
+#define CYGMEM_REGION_ram_SIZE (0x00800000)
 #define CYGMEM_REGION_ram_ATTR (CYGMEM_REGION_ATTR_R | CYGMEM_REGION_ATTR_W)
+#define CYGMEM_REGION_rom (0xC0000000)
+#define CYGMEM_REGION_rom_SIZE (0x01000000)
+#define CYGMEM_REGION_rom_ATTR (CYGMEM_REGION_ATTR_R)
 #ifndef __ASSEMBLER__
 extern char CYG_LABEL_NAME (__heap1) [];
 #endif
 #define CYGMEM_SECTION_heap1 (CYG_LABEL_NAME (__heap1))
-#define CYGMEM_SECTION_heap1_SIZE (0xc0000000 - (size_t) CYG_LABEL_NAME (__heap1))
+#define CYGMEM_SECTION_heap1_SIZE (CYGMEM_REGION_ram + CYGMEM_REGION_ram_SIZE - (size_t) CYG_LABEL_NAME (__heap1))
